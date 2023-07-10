9caa1b8fb66a2b52c2357786742ffad2633e844f
Jacob Brasil <jnxtbrasil@gmail.com>
2023-07-10 16:35:56 -0400

    Added Breakdown to Data Display Page

--- example/lib/pages/display_page.dart
+++ example/lib/pages/display_page.dart
@@ -1,5 +1,5 @@
 import 'package:bluejay/bluejay.dart';
-import 'package:bluejay_example/side_drawer.dart';
+import 'package:bluejay_example/widgets/side_drawer.dart';
 import 'package:flutter/material.dart';
 
 class DisplayPage extends StatefulWidget {
@@ -21,6 +21,31 @@ const BlueCodeBlock(
 \t],
 )''';
 
+  String breakdownCode = '''
+const BlueBreakdown(
+\ttitle: '128 GB',
+\tsubtitle: 'of 256 GB used',
+\tshowLabels: true,
+\ttotalValue: 256,
+\tsections: [
+\t\tBlueBreakdownSection(
+\t\t\tcolor: Colors.blue,
+\t\t\tlabel: 'Photos',
+\t\t\tvalue: 64,
+\t\t),
+\t\tBlueBreakdownSection(
+\t\t\tcolor: Colors.red,
+\t\t\tlabel: 'Applications',
+\t\t\tvalue: 32,
+\t\t),
+\t\tBlueBreakdownSection(
+\t\t\tcolor: Colors.green,
+\t\t\tlabel: 'System OS',
+\t\t\tvalue: 32,
+\t\t),
+\t],
+)''';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +53,7 @@ const BlueCodeBlock(
       body: CustomScrollView(
         slivers: [
           const SliverAppBar(
-            title: Text('Inputs'),
+            title: Text('Data Display'),
             pinned: true,
           ),
           SliverSafeArea(
@@ -61,6 +86,43 @@ const BlueCodeBlock(
                       language: CodeBlockLanguage.dart,
                       actions: [
                         BlueCopyButton(content: codeBlockCode),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    Text(
+                      'Breakdown',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(height: 16.0),
+                    const BlueBreakdown(
+                      title: '128 GB',
+                      subtitle: 'of 256 GB used',
+                      showLabels: true,
+                      totalValue: 256,
+                      sections: [
+                        BlueBreakdownSection(
+                          color: Colors.blue,
+                          label: 'Photos',
+                          value: 64,
+                        ),
+                        BlueBreakdownSection(
+                          color: Colors.red,
+                          label: 'Applications',
+                          value: 32,
+                        ),
+                        BlueBreakdownSection(
+                          color: Colors.green,
+                          label: 'System OS',
+                          value: 32,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    BlueCodeBlock(
+                      code: breakdownCode,
+                      language: CodeBlockLanguage.dart,
+                      actions: [
+                        BlueCopyButton(content: breakdownCode),
                       ],
                     ),
                   ],
